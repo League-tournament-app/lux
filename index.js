beb72d926bc3f880a44cb52981748d986b69cfb5
Dan Pantry <dan@lambdaexpression.io>
2015-09-15 18:02:25 +0100

    remove the NodeJS section
    We don't need this just yet until we are *far* further into the project.
    Maintaining it whilst we are still developing is just going to be a pain
    in the ass.

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
 'use strict';
-// Everything in this file is specifically for node-only environments i.e,
-// including this library from another library. The library will be transpiled to
-// ES5 via babel so that the caller need not use Babel. This file is not transpiled.
-exports.Sitemap = require('./lib/sitemap');
