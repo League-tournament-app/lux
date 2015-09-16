da3d8a810c2d0d532a46848a6d392a81f96a4326
Dan <dan.pantry@hotmail.com>
2015-09-16 09:13:50 +0000

    add home route component

---
+++ src/home/route.jsx
@@ -0,0 +1,10 @@
+'use strict';
+import React from 'react';
+
+class HomeRoute extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default HomeRoute;
--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,8 @@
 'use strict';
 import React from 'react';
 import {Router, Route} from 'react-router';
+import HomeRoute from './home/route';
 
 export default <Router>
+  <Route component={HomeRoute} path='/' />
 </Router>;
